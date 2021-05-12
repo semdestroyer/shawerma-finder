19b57f3db421e5908435a870ae5258e575e83395
Arsenii <semdestroyer@yandex.ru>
2021-05-12 21:14:57 +0300

    fix map

--- resources/views/map.blade.php
+++ resources/views/map.blade.php
@@ -3,6 +3,7 @@
 <head>
     <script src="https://api-maps.yandex.ru/2.1/?apikey=9d6b7590-f990-4826-8d49-9967a9cb1dbd&lang=ru_RU" type="text/javascript">
     </script>
+    <script src={{asset("js/ymaps.js")}} type="text/javascript"></script>
 </head>
 <body>
     <h1>
